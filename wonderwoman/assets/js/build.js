2fa902a6a729ca00f6389801637668f394965ff5
Benjamin Rochez <br@oncodna.com>
2017-10-20 13:26:55 +0200

    theming: added animation prev()

--- wonderwoman/assets/js/build.js
+++ wonderwoman/assets/js/build.js
@@ -35,9 +35,10 @@ var ExpandTransition = Barba.BaseTransition.extend({
 
   zoom: function() {
     var deferred = Barba.Utils.deferred();
-    console.log(elClicked);
+    var elAttr = elClicked.getAttribute('data-zindex');
     var tl = new TimelineMax();
-    tl.to(elClicked, 1,{className: '+=is-open', onComplete: function() {
+    tl.to(elClicked, 0, {css: {zIndex: elAttr}})
+    .to(elClicked, 1,{className: '+=is-open', onComplete: function() {
       deferred.resolve();
     }});
 
@@ -49,10 +50,39 @@ var ExpandTransition = Barba.BaseTransition.extend({
   }
 });
 
-Barba.Pjax.getTransition = function() {
-  var transitionObj = ExpandTransition;
 
-  //Barba.HistoryManager.prevStatus().namespace
+
+var BackTransition = Barba.BaseTransition.extend({
+  start: function() {
+    Promise
+      .all([this.newContainerLoading, this.zoom()])
+      .then(this.showNewPage.bind(this));
+  },
+
+  zoom: function() {
+    var deferred = Barba.Utils.deferred();
+
+    var tl = new TimelineMax();
+    tl.to('body', 1,{opacity: 0, onComplete: function() {
+      deferred.resolve();
+    }})
+    .to('body', 1, {opacity: 1});
+
+
+    return deferred.promise;
+  },
+
+  showNewPage: function() {
+    this.done();
+  }
+});
+
+Barba.Pjax.getTransition = function() {
+
+  var transitionObj = ExpandTransition;
+  if(Barba.HistoryManager.prevStatus().namespace === "Single") {
+     transitionObj = BackTransition;
+  }
   return transitionObj;
 };
 
